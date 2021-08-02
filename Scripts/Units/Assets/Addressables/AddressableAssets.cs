4125b11ca45b0fb59aaf87879b0007e18f5f4861
Slava Korzun <Hoodrij@gmail.com>
2021-08-02 19:39:41 +0300

    Update AddressableAssets.cs

--- Scripts/Units/Assets/Addressables/AddressableAssets.cs
+++ Scripts/Units/Assets/Addressables/AddressableAssets.cs
@@ -1,27 +1,43 @@
 ﻿#if ADDRESSABLES
 using System.Threading.Tasks;
+using UnityAsync;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Core.Units
 {
     public class AddressableAssets : Unit, IAssets
     {
+        private static string LOG_TAG = "[Addressables]".Color(Color.red);
+        
         public async Task<Object> Load(string path)
         {
-            return await Addressables.LoadAssetAsync<Object>(path).Task;
+            return await CheckExistenceAndLoad<Object>(path);
         }
         
         public async Task<T> Load<T>(string path) where T : Object
         {
             if (typeof(T).IsSubclassOf(typeof(Component)))
-            {
-                GameObject gameObject = await Addressables.LoadAssetAsync<GameObject>(path).Task;
+            { 
+                GameObject gameObject = await CheckExistenceAndLoad<GameObject>(path);
                 return gameObject.GetComponent<T>();
             }
         
-            return await Addressables.LoadAssetAsync<T>(path).Task;
+            return await CheckExistenceAndLoad<T>(path);
         }
+
+        private async Task<T> CheckExistenceAndLoad<T>(string path) where T : Object
+        {
+            AsyncOperationHandle<T> ao = Addressables.LoadAssetAsync<T>(path);
+            await Wait.While(() => ao.Status == AsyncOperationStatus.None);
+            if (ao.Status == AsyncOperationStatus.Failed)
+            {
+                $"{LOG_TAG} Path {path} does not exist".log();
+                return null;
+            }
+            return await ao.Task;
+        } 
     }
 }
 #endif
